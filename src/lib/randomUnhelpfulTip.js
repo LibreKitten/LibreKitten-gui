1dd6cb1896ef5264bd4b152467e6bf419e8b61de
o97doge <>
2024-01-27 11:40:47 +1100

    this should fix it

---
+++ src/lib/randomUnhelpfulTip.js
@@ -0,0 +1,11 @@
+export const tip = [
+    'LibreKitten was started at January 6, 2024.',
+    'Fact: Capybaras and cats rule, humans drool.',
+    'The name of the LibreKitten cat is "Librekitty".',
+    'Tip: If you\'re having trouble with your project, ask someone for help.',
+    'Tip: These tips are unhelpful.',
+    'Fact: Your code is the cat\'s meow, keep going.',
+    'Fact: This is a sentence.',
+    'Fact: Capybaras look chill, but they\'re actually not, they\'re planning WORLD DOMINATION.',
+    'Crazy? I was crazy once. They locked me in a room, a rubber room. A rubber room with rats. And rats make me crazy.'
+];
