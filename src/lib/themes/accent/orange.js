6893d73e8576c2aa1ed26cd7547c575a759353a5
o97doge <>
2024-03-05 17:31:11 +1100

    Fix orange.js

--- src/lib/themes/accent/orange.js
+++ src/lib/themes/accent/orange.js
@@ -6,14 +6,7 @@ const guiColors = {
     'looks-secondary': 'rgba(255, 130, 0, 1)',
     'looks-transparent': 'rgba(255, 130, 0, 0.35)',
     'looks-light-transparent': 'hgba(255, 130, 0, 0.15)',
-    'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
-
-    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
-    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
-    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
-    'extensions-light': 'hsla(10, 57%, 85%, 1)',
-
-    'drop-highlight': 'rgba(255, 130, 0, 1)'
+    'looks-secondary-dark': 'rgba(209, 108, 0, 1)',
 };
 
 const blockColors = {
