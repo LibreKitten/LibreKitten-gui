0d21ff2ad638c4a86a03129284f9876d07b3320a
o97doge <>
2024-03-14 16:22:31 +1100

    Use our own cloud server.

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GUI from '../containers/gui.jsx';
 
 const searchParams = new URLSearchParams(location.search);
-const cloudHost = searchParams.get('cloud_host') || null;
+const cloudHost = searchParams.get('cloud_host') || 'wss://cut-cool-rosehip.glitch.me';
 
 const RenderGUI = props => (
     <GUI
