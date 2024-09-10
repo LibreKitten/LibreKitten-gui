59329d68852e0095b5329c3ce0ca29ce8e89ce91
o97doge <>
2024-09-10 16:21:58 +1000

    Forgot to fix these.

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -37,7 +37,6 @@ import TWRestorePointManager from '../../containers/tw-restore-point-manager.jsx
 import TWFontsModal from '../../containers/tw-fonts-modal.jsx';
 import TWUnknownPlatformModal from '../../containers/tw-unknown-platform-modal.jsx';
 import TWInvalidProjectModal from '../../containers/tw-invalid-project-modal.jsx';
-import Window from '../../containers/lk-window.jsx';
 
 import {STAGE_SIZE_MODES, FIXED_WIDTH, UNCONSTRAINED_NON_STAGE_WIDTH} from '../../lib/layout-constants';
 import {resolveStageSize} from '../../lib/screen-utils';
